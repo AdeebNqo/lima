fd3daa66d6d13518431849162ff63d835827d454
AdeebNqo <adeebnqo@gmail.com>
2014-05-06 19:25:01 +0200

    done with cleanup script

--- dashboard/header-imports.php
+++ dashboard/header-imports.php
@@ -1,19 +1,19 @@
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 <!-- bootstrap 3.0.2 -->
-<link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/bootstrap.min.css');?> rel=stylesheet type=text/css />
 <!-- font Awesome -->
-<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/font-awesome.min.css');?> rel=stylesheet type=text/css />
 <!-- Ionicons -->
-<link href="css/ionicons.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/ionicons.min.css');?> rel=stylesheet type=text/css />
 <!-- Morris chart -->
-<link href="css/morris/morris.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/morris/morris.css');?> rel=stylesheet type=text/css />
 <!-- jvectormap -->
-<link href="css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/jvectormap/jquery-jvectormap-1.2.2.css');?> rel=stylesheet type=text/css />
 <!-- fullCalendar -->
-<link href="css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/fullcalendar/fullcalendar.css');?> rel=stylesheet type=text/css />
 <!-- Daterange picker -->
-<link href="css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/daterangepicker/daterangepicker-bs3.css');?> rel=stylesheet type=text/css />
 <!-- bootstrap wysihtml5 - text editor -->
-<link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css');?> rel=stylesheet type=text/css />
 <!-- Theme style -->
-<link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/css/AdminLTE.css');?> rel=stylesheet type=text/css />
